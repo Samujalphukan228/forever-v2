f7dbd0de089f35a292c07acfd8d9a582659917a8
Samujal Phukan <samujalphukan@yahoo.com>
2025-11-03 18:00:46 +0530

    Update BestSeller.jsx

--- forever.com-main/Frontend/src/components/BestSeller.jsx
+++ forever.com-main/Frontend/src/components/BestSeller.jsx
@@ -7,7 +7,6 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 gsap.registerPlugin(ScrollTrigger)
 
-
 const BestSeller = () => {
     const { products } = useContext(ShopContext)
     const [bestSeller, setBestSeller] = useState([])
